a7faff8fde7f08ec2167c162f5b7c3ab916760c2
Vikram-Nagpure <vikramnagpure99@gmail.com>
2024-07-23 12:32:18 +0530

    uplode

--- Navigator/src/Components/Navigation.jsx
+++ Navigator/src/Components/Navigation.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 function Navigation() {
   return (
     <div style={{justifyContent:"space-between",display:"flex",textDecoration:"none"}}>
-      <Link to={"/"}>home</Link>
+      <Link to={"/"}>Home</Link>
       <Link to={"/product"}>product</Link>
-      <Link to={"/addproduct"}>Addproduct</Link>
+      <Link to={"/addproduct"}>About</Link>
       <Link to={"/login"}>login</Link>
     </div>
   )
